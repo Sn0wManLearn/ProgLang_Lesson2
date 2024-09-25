eb37b4e9c6fe6fc62909b1082c9a0fd4f5e9a0ef
Bl1nker <Alex_001@sibmail.com>
2024-09-25 15:54:49 +0700

    Task 1 done

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class MyClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число");
+        if (!Int32.TryParse(Console.ReadLine(), out int num))
+        {
+            Console.WriteLine("Число введено не корректно.");
+            return;
+        }
+        if (num % 7 == 0 && num % 23 == 0) Console.WriteLine($"Число {num} кратно 7 и 23");
+        else Console.WriteLine($"Число {num} некратно 7 и 23");
+    }
+}
