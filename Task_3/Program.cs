183fe6d52d3d91bb9b1de5f33caec0842d619b99
Bl1nker <Alex_001@sibmail.com>
2024-09-25 16:22:56 +0700

    Task 3 done

---
+++ Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class MyClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число от 10 до 99.");
+        string? input = Console.ReadLine();
+        if (input == null)
+        {
+            Console.WriteLine("Введена пустая строка.");
+            return;
+        }
+        if (!int.TryParse(input, out int num))
+        {
+            Console.WriteLine("Число введено не корректно");
+            return;
+        }
+        if (num < 10 || num > 99)
+        {
+            Console.WriteLine("Введенное число не входит в заданный диапазон.");
+            return;
+        }
+        int digit1 = num / 10;
+        int digit2 = num % 10;
+
+        int res = digit1 > digit2 ? digit1 : digit2;
+        Console.WriteLine($"Во введенном числе {num} наибольшей является цифра {res}.");
+    }
+}
