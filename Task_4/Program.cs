1d9d57f5f907a8cdcc85654e8079dae953ae4532
Bl1nker <Alex_001@sibmail.com>
2024-09-25 16:38:30 +0700

    Task 4 done. All tasks done

---
+++ Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class MyClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число.");
+        string? input = Console.ReadLine();
+        if (input == null)
+        {
+            Console.WriteLine("Введена пустая строка.");
+            return;
+        }
+
+        if (Int32.TryParse(input, out int _))
+        {
+            char[] chars = input.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.Write($"{chars[i]}");
+                if (i != chars.Length - 1) Console.Write(", ");
+            }
+        }
+    }
+}
